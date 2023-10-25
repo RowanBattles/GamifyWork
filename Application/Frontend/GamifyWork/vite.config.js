5c044d58bbe86b78a927066ef1822dfd15b1cc83
Rowan <rowanvanderweel@gmail.com>
2023-10-25 23:12:12 +0200

    Workflow bugs

--- Application/Frontend/GamifyWork/vite.config.js
+++ Application/Frontend/GamifyWork/vite.config.js
@@ -5,8 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
-    cache: false,
-    clearMocks: true,
+    coverage: {
+      reporter: ["lcov"],
+    },
+
     globals: true,
     environment: "jsdom",
     setupFiles: "./tests/setup.js",
