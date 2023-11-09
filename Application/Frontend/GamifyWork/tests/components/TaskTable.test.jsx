ae0aab355d90b285e6391615b95a391ab4b1616e
Rowan <rowanvanderweel@gmail.com>
2023-11-09 15:36:11 +0100

    tests frontend

--- Application/Frontend/GamifyWork/tests/components/TaskTable.test.jsx
+++ Application/Frontend/GamifyWork/tests/components/TaskTable.test.jsx
@@ -1,69 +1,112 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import TaskTable from "../../src/components/TaskTable";
+import { CreateTask } from "../../src/utils/api";
+import { describe } from "vitest";
 
-test("renders TaskTable component", () => {
-  const tasks = [
-    {
-      task_ID: 1,
-      title: "Task 1",
-      description: "Description 1",
-      completed: false,
-    },
-    {
-      task_ID: 2,
-      title: "Task 2",
-      description: "Description 2",
-      completed: true,
-    },
-  ];
+describe("renderingTaskTable", () => {
+  test("renders TaskTable component", () => {
+    const tasks = [
+      {
+        task_ID: 1,
+        title: "Task 1",
+        description: "Description 1",
+        completed: false,
+      },
+      {
+        task_ID: 2,
+        title: "Task 2",
+        description: "Description 2",
+        completed: true,
+      },
+    ];
 
-  render(<TaskTable tasks={tasks} title="Test Title" />);
+    render(<TaskTable tasks={tasks} title="Test Title" />);
 
-  // Check if the title is rendered
-  const titleElement = screen.getByText("Test Title");
-  expect(titleElement).toBeInTheDocument();
+    // Check if the title is rendered
+    const titleElement = screen.getByText("Test Title");
+    expect(titleElement).toBeInTheDocument();
 
-  // Check if the All, Active, Completed filters are rendered
-  const allFilter = screen.getByText("All");
-  const activeFilter = screen.getByText("Active");
-  const completedFilter = screen.getByText("Completed");
-  expect(allFilter).toBeInTheDocument();
-  expect(activeFilter).toBeInTheDocument();
-  expect(completedFilter).toBeInTheDocument();
+    // Check if the All, Active, Completed filters are rendered
+    const allFilter = screen.getByText("All");
+    const activeFilter = screen.getByText("Active");
+    const completedFilter = screen.getByText("Completed");
+    expect(allFilter).toBeInTheDocument();
+    expect(activeFilter).toBeInTheDocument();
+    expect(completedFilter).toBeInTheDocument();
 
-  // Check if tasks are rendered
-  const task1Element = screen.getByText("Task 1");
-  const task2Element = screen.getByText("Task 2");
-  expect(task1Element).toBeInTheDocument();
-  expect(task2Element).toBeInTheDocument();
+    // Check if tasks are rendered
+    const task1Element = screen.getByText("Task 1");
+    const task2Element = screen.getByText("Task 2");
+    expect(task1Element).toBeInTheDocument();
+    expect(task2Element).toBeInTheDocument();
+  });
+
+  test("filters tasks by status", () => {
+    const tasks = [
+      {
+        task_ID: 1,
+        title: "Task 1",
+        description: "Description 1",
+        completed: false,
+      },
+      {
+        task_ID: 2,
+        title: "Task 2",
+        description: "Description 2",
+        completed: true,
+      },
+    ];
+
+    render(<TaskTable tasks={tasks} title="Test Title" />);
+
+    // Click on the Active filter
+    const activeFilter = screen.getByText("Active");
+    fireEvent.click(activeFilter);
+
+    // Check if only the active task is rendered
+    const task1Element = screen.getByText("Task 1");
+    const task2Element = screen.queryByText("Task 2");
+    expect(task1Element).toBeInTheDocument();
+    expect(task2Element).toBeNull();
+  });
 });
 
-test("filters tasks by status", () => {
-  const tasks = [
-    {
-      task_ID: 1,
-      title: "Task 1",
-      description: "Description 1",
+describe("addingTask", () => {
+  vi.mock("../../src/utils/api", () => ({
+    CreateTask: vi.fn(),
+  }));
+
+  test("handleInputChange updates newTask state correctly", () => {
+    const { getByPlaceholderText } = render(
+      <TaskTable tasks={[]} title="Recurring" />
+    );
+    const inputElement = getByPlaceholderText("Add task");
+
+    fireEvent.change(inputElement, { target: { value: "New Task" } });
+
+    expect(inputElement.value).toBe("New Task");
+  });
+
+  test("handleKeyDown function works correctly", async () => {
+    CreateTask.mockResolvedValueOnce({});
+
+    render(<TaskTable tasks={[]} title="Recurring" />);
+
+    const textarea = screen.getByPlaceholderText("Add task");
+    fireEvent.change(textarea, { target: { value: "New Task" } });
+    fireEvent.keyDown(textarea, { key: "Enter", code: "Enter" });
+
+    expect(CreateTask).toHaveBeenCalledWith({
+      title: "New Task",
+      description: null,
+      points: 0,
       completed: false,
-    },
-    {
-      task_ID: 2,
-      title: "Task 2",
-      description: "Description 2",
-      completed: true,
-    },
-  ];
-
-  render(<TaskTable tasks={tasks} title="Test Title" />);
-
-  // Click on the Active filter
-  const activeFilter = screen.getByText("Active");
-  fireEvent.click(activeFilter);
-
-  // Check if only the active task is rendered
-  const task1Element = screen.getByText("Task 1");
-  const task2Element = screen.queryByText("Task 2");
-  expect(task1Element).toBeInTheDocument();
-  expect(task2Element).toBeNull();
+      recurring: true,
+      recurrenceType: null,
+      recurrenceInterval: null,
+      nextDueDate: null,
+      user_ID: 1,
+    });
+  });
 });
--- Application/Frontend/GamifyWork/tests/utils/api.test.jsx
+++ Application/Frontend/GamifyWork/tests/utils/api.test.jsx
@@ -1,5 +1,5 @@
 import { test, expect, describe, vi } from "vitest";
-import { getTasks } from "../../src/utils/api";
+import { getTasks, CreateTask, getRewards } from "../../src/utils/api";
 import axios from "axios";
 
 vi.mock("axios");
@@ -22,6 +22,81 @@ describe("getTasks", () => {
 
     try {
       await getTasks();
+    } catch (error) {
+      expect(error.message).toEqual(errorMessage);
+    }
+  });
+});
+
+describe("CreateTask", () => {
+  test("should create a new task", async () => {
+    const taskData = {
+      title: "New Task",
+      description: null,
+      points: 0,
+      completed: false,
+      recurring: false,
+      recurrenceType: null,
+      recurrenceInterval: null,
+      nextDueDate: null,
+      user_ID: 1,
+    };
+
+    const responseData = { id: 1, ...taskData };
+
+    axios.post.mockResolvedValueOnce({ data: responseData });
+
+    const createdTask = await CreateTask(taskData);
+
+    expect(createdTask).toEqual(responseData);
+    expect(axios.post).toHaveBeenCalledWith(expect.any(String), taskData);
+  });
+
+  test("should throw an error if request fails", async () => {
+    const taskData = {
+      title: "New Task",
+      description: null,
+      points: 0,
+      completed: false,
+      recurring: false,
+      recurrenceType: null,
+      recurrenceInterval: null,
+      nextDueDate: null,
+      user_ID: 1,
+    };
+
+    const errorMessage = "Failed to create task";
+
+    axios.post.mockRejectedValueOnce(new Error(errorMessage));
+
+    try {
+      await CreateTask(taskData);
+    } catch (error) {
+      expect(error.message).toBe(errorMessage);
+    }
+
+    expect(axios.post).toHaveBeenCalledWith(expect.any(String), taskData);
+  });
+});
+
+describe("getRewards", () => {
+  test("should return an array of rewards", async () => {
+    const mockData = [
+      { id: 1, title: "Reward 1" },
+      { id: 2, title: "Reward 2" },
+    ];
+    axios.get.mockResolvedValue({ data: mockData });
+
+    const rewards = await getRewards();
+    expect(rewards).toEqual(mockData);
+  });
+
+  test("getRewards handles errors", async () => {
+    const errorMessage = "An error occurred";
+    axios.get.mockRejectedValue(new Error(errorMessage));
+
+    try {
+      await getRewards();
     } catch (error) {
       expect(error.message).toEqual(errorMessage);
     }
